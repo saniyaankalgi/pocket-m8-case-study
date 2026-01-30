851626de712aa88d41359d39bc682bd046492720
saniyaankalgi <165926112+saniyaankalgi@users.noreply.github.com>
2026-01-30 13:38:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const toc = document.getElementById("toc-list");
-const headings = document.querySelectorAll("h2");
 
-headings.forEach((heading, index) => {
-  const id = `section-${index}`;
-  heading.id = id;
-
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = `#${id}`;
-  a.textContent = heading.textContent;
-
-  li.appendChild(a);
-  toc.appendChild(li);
-});
